86c99761e791c4bcca3a354293de08336c520411
ersultanbek <ersultan_elgondin@mail.ru>
2018-12-08 00:11:32 +0600

    My changes

---
+++ src/app/services/offerService.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from './HttpService';
+
+@Injectable()
+export class OfferService {
+
+    constructor(private httpService : HttpService)
+    {}
+
+    createOffer(){
+
+
+    }
+
+    getAllOffers(){}
+
+    getOfferById(){}
+
+    deleteOffers(){}
+
+}
--- src/app/variables/SERVER_API_URL.ts
+++ src/app/variables/SERVER_API_URL.ts
@@ -1,3 +1,5 @@
 export class SERVER_API_URL{
     public static LOGIN_URL = '/auth/login';
+    public static GET_ALL_OFFERS = "/offers/get-all";
+    public static GET_OFFER_BY_ID = "/offers/get-by-id";
 }
